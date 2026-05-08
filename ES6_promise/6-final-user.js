8bff3e970c79a317ae7256461b77b59a614ffbeb
nobulp <lucaslupon873@gmail.com>
2026-05-08 21:04:05 +0200

    fix: Update value handling in handleProfileSignup to ensure proper error messaging

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.value || result.reason,
+      value: result.value !== undefined ? result.value : result.reason.toString(),
     })));
 }
